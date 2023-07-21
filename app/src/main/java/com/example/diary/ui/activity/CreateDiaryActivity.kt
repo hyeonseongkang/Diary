cd864ed975f1bb3ee676f5ad8e8e25fedbfd09dd
HyeonSeongKang <gt9697@naver.com>
2023-07-21 23:22:47 +0900

    feat: 다이어리 작성

--- app/src/main/java/com/example/diary/ui/activity/CreateDiaryActivity.kt
+++ app/src/main/java/com/example/diary/ui/activity/CreateDiaryActivity.kt
@@ -3,10 +3,30 @@ package com.example.diary.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.diary.R
+import com.example.diary.databinding.ActivityCreateDiaryBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class CreateDiaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateDiaryBinding
+
+    val database = Firebase.database
+    val myRef = database.getReference("diary")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_diary)
+        binding = ActivityCreateDiaryBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+
+    fun btnClick() {
+        binding.btnSave.setOnClickListener {
+            val diary = binding.tvDiary.text
+            myRef.setValue(diary)
+        }
     }
 }
