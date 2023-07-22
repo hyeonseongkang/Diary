731b13e79f0ccf8c4efca5562a537742fe209dce
HyeonSeongKang <gt9697@naver.com>
2023-07-22 21:22:34 +0900

    feat: getDiaryList

---
+++ app/src/main/java/com/example/diary/data/Diary.kt
@@ -0,0 +1,6 @@
+package com.example.diary.data
+
+import java.util.Date
+
+data class Diary(var id: String, var writer: String, var diary: String, var date: Date) {
+}
--- app/src/main/java/com/example/diary/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/diary/ui/fragment/HomeFragment.kt
@@ -2,17 +2,30 @@ package com.example.diary.ui.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.diary.data.Diary
 import com.example.diary.databinding.FragmentHomeBinding
 import com.example.diary.ui.activity.CreateDiaryActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    val database = Firebase.database
+    val myRef = database.getReference("diary")
+
+    val diaryList: MutableList<Diary> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +49,24 @@ class HomeFragment: Fragment() {
             val intent = Intent(context, CreateDiaryActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun getDiaryData() {
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (snapshot1 in snapshot.children) {
+                    val diary: Diary? = snapshot1.getValue(Diary::class.java)
+                    diaryList.add(diary!!)
+                }
+               // Log.d(TAG, "Value is: " + value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+               // Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
     }
 
     override fun onDestroyView() {
